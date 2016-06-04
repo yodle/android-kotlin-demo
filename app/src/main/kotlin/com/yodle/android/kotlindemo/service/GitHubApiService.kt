1a2eb2f94c17fb633b042e9980bb41f4eebb3f25
Dan Nizri <dsn5ft@virginia.edu>
2016-06-04 10:25:46 -0400

    add repository detail activity

--- app/src/main/kotlin/com/yodle/android/kotlindemo/service/GitHubApiService.kt
+++ app/src/main/kotlin/com/yodle/android/kotlindemo/service/GitHubApiService.kt
@@ -1,7 +1,9 @@
 package com.yodle.android.kotlindemo.service
 
+import com.yodle.android.kotlindemo.model.Repository
 import com.yodle.android.kotlindemo.model.RepositorySearchResults
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 import rx.Observable
 
@@ -9,4 +11,8 @@ interface GitHubApiService {
 
     @GET("search/repositories")
     fun searchRepositories(@Query("q") query: String): Observable<RepositorySearchResults>
+
+    @GET("/repos/{owner}/{repo}")
+    fun getRepository(@Path("owner") owner: String, @Path("repo") repository: String): Observable<Repository>
+
 }
