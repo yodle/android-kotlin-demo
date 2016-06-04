9ec36b98994bacd2ed664cdd586aa9c356d393f2
Dan Nizri <daniel.nizri@yodle.com>
2016-06-03 21:45:19 -0400

    Implement search repositories screen

---
+++ app/src/main/kotlin/com/yodle/android/kotlindemo/view/SearchCardView.kt
@@ -0,0 +1,32 @@
+package com.yodle.android.kotlindemo.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.FrameLayout
+import com.yodle.android.kotlindemo.R
+import kotlinx.android.synthetic.main.search_card.view.*
+
+class SearchCardView : FrameLayout {
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(context)
+    }
+
+    constructor(context: Context) : super(context) {
+        init(context)
+    }
+
+    fun init(context: Context) {
+        LayoutInflater.from(context).inflate(R.layout.search_card, this, true)
+
+        searchCardClear.setOnClickListener { searchCardEditText.text = null }
+    }
+
+    fun getEditText(): EditText = searchCardEditText
+}
