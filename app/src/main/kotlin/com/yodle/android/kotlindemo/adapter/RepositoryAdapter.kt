c4fe694c5f055be3b75a2d171bc2963cfeb0c1c2
Dan Nizri <dsn5ft@virginia.edu>
2016-06-05 02:38:18 -0400

    extension functions for adapter

--- app/src/main/kotlin/com/yodle/android/kotlindemo/adapter/RepositoryAdapter.kt
+++ app/src/main/kotlin/com/yodle/android/kotlindemo/adapter/RepositoryAdapter.kt
@@ -5,28 +5,25 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import com.ocpsoft.pretty.time.PrettyTime
-import com.squareup.picasso.Picasso
 import com.yodle.android.kotlindemo.R
 import com.yodle.android.kotlindemo.activity.RepositoryDetailActivity
+import com.yodle.android.kotlindemo.extension.format
+import com.yodle.android.kotlindemo.extension.formatted
 import com.yodle.android.kotlindemo.extension.inflateLayout
+import com.yodle.android.kotlindemo.extension.loadUrl
 import com.yodle.android.kotlindemo.model.Repository
 import kotlinx.android.synthetic.main.repository_item.view.*
-import org.joda.time.DateTime
-import java.text.NumberFormat
+import java.util.*
 
 class RepositoryAdapter(val context: Context) : RecyclerView.Adapter<RepositoryAdapter.RepositoryViewHolder>() {
 
-    var repositories: List<Repository> = arrayListOf()
+    var repositories: List<Repository> = ArrayList()
 
     override fun getItemCount() = repositories.size
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepositoryViewHolder {
-        return RepositoryViewHolder(context.inflateLayout(R.layout.repository_item, parent, false))
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = RepositoryViewHolder(context.inflateLayout(R.layout.repository_item, parent))
 
-    override fun onBindViewHolder(holder: RepositoryViewHolder, position: Int) {
-        holder.bind(repositories.get(position))
-    }
+    override fun onBindViewHolder(holder: RepositoryViewHolder, position: Int) = holder.bind(repositories.get(position))
 
     class RepositoryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
@@ -34,18 +31,12 @@ class RepositoryAdapter(val context: Context) : RecyclerView.Adapter<RepositoryA
         val prettyTime = PrettyTime()
 
         fun bind(repository: Repository) {
-            val updatedTime = prettyTime.format(DateTime(repository.pushed_at).toDate());
-
             itemView.repositoryItemTitle.text = repository.full_name
             itemView.repositoryItemDescription.text = repository.description
-            itemView.repositoryItemLastUpdated.text = "Updated $updatedTime"
-            itemView.repositoryItemStarCount.text = NumberFormat.getInstance().format(repository.watchers_count)
-
-            Picasso.with(context).load(repository.owner.avatar_url).into(itemView.repositoryItemImage)
-
-            itemView.repositoryItemRootLayout.setOnClickListener {
-                context.startActivity(RepositoryDetailActivity.getIntent(context, repository))
-            }
+            itemView.repositoryItemLastUpdated.text = "Updated ${prettyTime.format(repository.pushed_at)}"
+            itemView.repositoryItemStarCount.text = repository.watchers_count.formatted()
+            itemView.repositoryItemImage.loadUrl(repository.owner.avatar_url)
+            itemView.repositoryItemRootLayout.setOnClickListener { context.startActivity(RepositoryDetailActivity.getIntent(context, repository)) }
         }
     }
 }
